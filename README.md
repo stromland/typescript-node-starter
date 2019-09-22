0166ed6c81e84e744bfd4631d0956a39e1600119
Espen Strømland <espen.stromland@gmail.com>
2019-09-22 09:47:18 +0200

    build with babel

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ Starter project for NodeJS projects
 - Testing with Jest
 - Linting with ESLint
 - Formatting with Prettier
-- TypeScript compiler for building the project
+- Building with Babel
---
+++ babel.config.js
@@ -0,0 +1,23 @@
+module.exports = api => {
+  api.cache(true);
+  return {
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          targets: {
+            node: 'current'
+          }
+        }
+      ],
+      '@babel/preset-typescript'
+    ],
+    plugins: ['@babel/plugin-proposal-class-properties'],
+    env: {
+      build: {
+        ignore: ['**/*.test.ts', '__snapshots__']
+      }
+    },
+    ignore: ['node_modules']
+  };
+};
