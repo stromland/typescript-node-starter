fec4b19ac8a18afd26b93723a602fd23c0eb97eb
Espen Strømland <Espen.Stromland@skatteetaten.no>
2021-12-08 23:22:30 +0100

    updated deps

--- src/main/index.ts
+++ src/main/index.ts
@@ -2,6 +2,5 @@ export const greet = (name = 'world'): string => `Hello, ${name}!`;
 
 export function getNameFromArgs(): string {
   const args = process.argv;
-  // node index.js <name>
   return args.length > 2 ? args[2] : 'world';
 }
